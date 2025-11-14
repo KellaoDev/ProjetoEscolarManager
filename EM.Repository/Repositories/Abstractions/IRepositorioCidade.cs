27e75f2f1bacdcfe52817da3c3d2cdfd830a0375
KellaoDev <keliofilho12@gmail.com>
2025-11-13 22:51:39 -0300

    feat(repository): Adiciona validação para ver se existe nome de cidade e código IBGE cadastrado no banco de dados

--- EM.Repository/Repositories/Abstractions/IRepositorioCidade.cs
+++ EM.Repository/Repositories/Abstractions/IRepositorioCidade.cs
@@ -5,5 +5,7 @@ namespace EM.Repository.Repositories.Abstractions
     public interface IRepositorioCidade : IRepositorioAbstrato<Cidade>
     {
         bool PossuiRegistro(int id);
+        bool DescricaoExiste(string descricao, int? codigoDesconsiderar = null);
+        bool CodigoIbgeExiste(int? codigoIbge, int? codigoDesconsiderar = null);
     }
 }
