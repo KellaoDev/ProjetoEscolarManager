808dd4df6efa867f8a5b4f0a293cba9a8292c4a4
KellaoDev <keliofilho12@gmail.com>
2025-11-15 14:04:32 -0300

    feat(domain): Adiciona nova classe de parâmetros para relatório do aluno

---
+++ EM.Domain/ParametrosRelatorioAluno.cs
@@ -0,0 +1,10 @@
+﻿namespace EM.Domain
+{
+    public class ParametrosRelatorioAluno
+    {
+        public bool EhParaEmitirCidade { get; set; }
+        public bool EhParaEmitirSexo { get; set; }
+        public bool EhParaEmitirCpf { get; set; }
+        public bool EhParaEmitirDataNascimento { get; set; }
+    }
+}
