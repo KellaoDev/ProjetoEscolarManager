1f085155385688b0ba415a131abf933ff7f4fd6e
KellaoDev <keliofilho12@gmail.com>
2025-11-15 14:08:12 -0300

    feat(controllers): Adiciona controler RelatorioAluno

---
+++ EM.Web/Controllers/RelatorioAlunoController.cs
@@ -0,0 +1,29 @@
+﻿using EM.Domain;
+using EM.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EM.Web.Controllers
+{
+    public class RelatorioAlunoController(IRelatorioAluno relatorioAluno) : Controller
+    {
+        private readonly IRelatorioAluno _relatorioAlunos = relatorioAluno;
+
+        [HttpGet("RelatorioAluno")]
+        public IActionResult RelatorioDeAluno()
+        {
+            return View(new ParametrosRelatorioAluno());
+        }
+
+        public IActionResult EmitirRelatorio(ParametrosRelatorioAluno parametros)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Index", parametros);
+            }
+
+            byte[] pdfBytes = _relatorioAlunos.Emita(parametros);
+
+            return File(pdfBytes, "application/pdf", "RelatorioAlunos.pdf");
+        }
+    }
+}
