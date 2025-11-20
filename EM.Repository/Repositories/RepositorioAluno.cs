1dcd3f02c3da7596d38b8983ac7cdf7f059c3884
KellaoDev <keliofilho12@gmail.com>
2025-11-19 22:14:16 -0300

    refactor(repository): Remove try catch

--- EM.Repository/Repositories/RepositorioAluno.cs
+++ EM.Repository/Repositories/RepositorioAluno.cs
@@ -12,57 +12,41 @@ namespace EM.Repository.Repositories
     {
         public void Add(Aluno aluno)
         {
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText =
+            cmd.CommandText =
                     @"INSERT INTO TBALUNO (ALUNNOME, ALUNCPF, ALUNDTNASC, ALUNSEXO, CIDACODIGO)
                                       VALUES (@ALUNNOME, @ALUNCPF, @ALUNDTNASC, @ALUNSEXO, @CIDACODIGO)";
 
-                cmd.Parameters.CreateParameter("@ALUNNOME", aluno.Nome);
-                cmd.Parameters.CreateParameter("@ALUNCPF", aluno.Cpf != null ? aluno.Cpf : DBNull.Value);
-                cmd.Parameters.CreateParameter("@ALUNDTNASC", aluno.DataNascimento);
-                cmd.Parameters.CreateParameter("@ALUNSEXO", aluno.EnumeradorSexo);
-                cmd.Parameters.CreateParameter("@CIDACODIGO", aluno.CidadeId);
+            cmd.Parameters.CreateParameter("@ALUNNOME", aluno.Nome);
+            cmd.Parameters.CreateParameter("@ALUNCPF", aluno.Cpf != null ? aluno.Cpf : DBNull.Value);
+            cmd.Parameters.CreateParameter("@ALUNDTNASC", aluno.DataNascimento);
+            cmd.Parameters.CreateParameter("@ALUNSEXO", aluno.EnumeradorSexo);
+            cmd.Parameters.CreateParameter("@CIDACODIGO", aluno.CidadeId);
 
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível adicionar o aluno.", ex);
-            }
+            cmd.ExecuteNonQuery();
         }
 
         public void Remove(Aluno aluno)
         {
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText = "DELETE FROM TBALUNO WHERE ALUNMATRICULA = @ALUNMATRICULA";
+            cmd.CommandText = "DELETE FROM TBALUNO WHERE ALUNMATRICULA = @ALUNMATRICULA";
 
-                cmd.Parameters.CreateParameter("@ALUNMATRICULA", aluno.Matricula);
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Nao foi possível remover o aluno.", ex);
-            }
+            cmd.Parameters.CreateParameter("@ALUNMATRICULA", aluno.Matricula);
+            cmd.ExecuteNonQuery();
         }
-
+        
         public void Update(Aluno aluno)
         {
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbTransaction tran = cn.BeginTransaction();
-                using DbCommand cmd = cn.CreateCommand();
-                cmd.Transaction = tran;
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbTransaction tran = cn.BeginTransaction();
+            using DbCommand cmd = cn.CreateCommand();
+            cmd.Transaction = tran;
 
-                cmd.CommandText = @"UPDATE TBALUNO SET
+            cmd.CommandText = @"UPDATE TBALUNO SET
                                        ALUNNOME = @ALUNNOME,
                                        ALUNCPF = @ALUNCPF,
                                        ALUNDTNASC = @ALUNDTNASC,
@@ -70,131 +54,90 @@ namespace EM.Repository.Repositories
                                        CIDACODIGO = @CIDACODIGO
                                        WHERE ALUNMATRICULA = @ALUNMATRICULA";
 
-                cmd.Parameters.CreateParameter("@ALUNMATRICULA", aluno.Matricula);
-                cmd.Parameters.CreateParameter("@ALUNNOME", aluno.Nome);
-                cmd.Parameters.CreateParameter("@ALUNCPF", aluno.Cpf != null ? aluno.Cpf : DBNull.Value);
-                cmd.Parameters.CreateParameter("@ALUNDTNASC", aluno.DataNascimento);
-                cmd.Parameters.CreateParameter("@ALUNSEXO", aluno.EnumeradorSexo);
-                cmd.Parameters.CreateParameter("@CIDACODIGO", aluno.CidadeId);
+            cmd.Parameters.CreateParameter("@ALUNMATRICULA", aluno.Matricula);
+            cmd.Parameters.CreateParameter("@ALUNNOME", aluno.Nome);
+            cmd.Parameters.CreateParameter("@ALUNCPF", aluno.Cpf != null ? aluno.Cpf : DBNull.Value);
+            cmd.Parameters.CreateParameter("@ALUNDTNASC", aluno.DataNascimento);
+            cmd.Parameters.CreateParameter("@ALUNSEXO", aluno.EnumeradorSexo);
+            cmd.Parameters.CreateParameter("@CIDACODIGO", aluno.CidadeId);
 
-                cmd.ExecuteNonQuery();
-                tran.Commit();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível atualizar o aluno.", ex);
-            }
+            cmd.ExecuteNonQuery();
+            tran.Commit();
         }
 
         public IEnumerable<Aluno> GetAll()
         {
-            try
-            {
-                List<Aluno> listaAlunos = [];
+            List<Aluno> listaAlunos = [];
 
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText = @"SELECT ALUNMATRICULA, ALUNNOME, ALUNCPF, ALUNDTNASC, ALUNSEXO,
+            cmd.CommandText = @"SELECT ALUNMATRICULA, ALUNNOME, ALUNCPF, ALUNDTNASC, ALUNSEXO,
                                        TBALUNO.CIDACODIGO, CIDADESCRICAO, CIDAUF, CIDACODIGOIBGE
                                        FROM TBALUNO
                                        INNER JOIN TBCIDADE ON TBALUNO.CIDACODIGO = TBCIDADE.CIDACODIGO
                                        ORDER BY ALUNMATRICULA";
 
-                using DbDataReader dr = cmd.ExecuteReader();
-                while (dr.Read())
+            using DbDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Aluno aluno = new()
                 {
-                    Aluno aluno = new()
+                    Matricula = dr.GetInt32(dr.GetOrdinal("ALUNMATRICULA")),
+                    Nome = dr.GetString(dr.GetOrdinal("ALUNNOME")),
+                    Cpf = dr.GetString(dr.GetOrdinal("ALUNCPF")),
+                    DataNascimento = dr.GetDateTime(dr.GetOrdinal("ALUNDTNASC")),
+                    EnumeradorSexo = (EnumeradorSexo)dr.GetInt32(dr.GetOrdinal("ALUNSEXO")),
+                    CidadeId = dr.GetInt32(dr.GetOrdinal("CIDACODIGO")),
+                    Cidade = new Cidade
                     {
-                        Matricula = dr.GetInt32(dr.GetOrdinal("ALUNMATRICULA")),
-                        Nome = dr.GetString(dr.GetOrdinal("ALUNNOME")),
-                        Cpf = dr.GetString(dr.GetOrdinal("ALUNCPF")),
-                        DataNascimento = dr.GetDateTime(dr.GetOrdinal("ALUNDTNASC")),
-                        EnumeradorSexo = (EnumeradorSexo)dr.GetInt32(dr.GetOrdinal("ALUNSEXO")),
-                        CidadeId = dr.GetInt32(dr.GetOrdinal("CIDACODIGO")),
-                        Cidade = new Cidade
-                        {
-                            Codigo = dr.GetInt32(dr.GetOrdinal("CIDACODIGO")),
-                            Descricao = dr.GetString(dr.GetOrdinal("CIDADESCRICAO")),
-                            EnumeradorUF = (EnumeradorUF)dr.GetInt32(dr.GetOrdinal("CIDAUF")),
-                            CodigoIBGE = dr.IsDBNull(dr.GetOrdinal("CIDACODIGOIBGE"))
+                        Codigo = dr.GetInt32(dr.GetOrdinal("CIDACODIGO")),
+                        Descricao = dr.GetString(dr.GetOrdinal("CIDADESCRICAO")),
+                        EnumeradorUF = (EnumeradorUF)dr.GetInt32(dr.GetOrdinal("CIDAUF")),
+                        CodigoIBGE = dr.IsDBNull(dr.GetOrdinal("CIDACODIGOIBGE"))
                             ? null
                             : dr.GetInt32(dr.GetOrdinal("CIDACODIGOIBGE"))
-                        }
-                    };
-
-                    listaAlunos.Add(aluno);
-                }
-                return listaAlunos;
+                    }
+                };
+                listaAlunos.Add(aluno);
             }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível todos os listaAlunos", ex);
-            }
+            return listaAlunos;
         }
 
         public IEnumerable<Aluno> Get(Expression<Func<Aluno, bool>> predicate)
         {
-            try
-            {
-                IEnumerable<Aluno> alunos = GetAll().Where(predicate.Compile());
-                return alunos;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível obter os alunos por predicado", ex);
-            }
+            IEnumerable<Aluno> alunos = GetAll().Where(predicate.Compile());
+            return alunos;
         }
 
         public IEnumerable<Aluno> GetByContendoNoNome(string contendoNome)
         {
-            try
-            {
-                IEnumerable<Aluno> alunos = GetAll().Where(a => a.Nome.Contains(contendoNome, StringComparison.OrdinalIgnoreCase));
-                return alunos;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível obter os alunos por parte do nome", ex);
-            }
+            IEnumerable<Aluno> alunos = GetAll().Where(a => a.Nome.Contains(contendoNome, StringComparison.OrdinalIgnoreCase));
+            return alunos;
         }
-
         public Aluno GetByMatricula(int matricula)
         {
-            try
-            {
-                Aluno? aluno = GetAll().FirstOrDefault(a => a.Matricula == matricula);
-                return aluno ?? throw new InvalidOperationException("Não foi possível encontrar o aluno por matrícula");
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível obter o aluno por matrícula", ex);
-            }
+            Aluno? aluno = GetAll().FirstOrDefault(a => a.Matricula == matricula);
+            return aluno ?? throw new InvalidOperationException("Não foi possível encontrar o aluno por matrícula");
         }
 
         public bool CpfExiste(string? cpf, int? matriculaDesconsiderar = null)
         {
             if (string.IsNullOrWhiteSpace(cpf))
                 return false;
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
                 
-                cmd.CommandText = @"SELECT COUNT(1) 
+            cmd.CommandText = @"SELECT COUNT(1) 
                                     FROM TBALUNO
                                     WHERE ALUNCPF = @ALUNCPF
                                     AND (@MATRICULA IS NULL OR ALUNMATRICULA <> @MATRICULA)";
-                cmd.Parameters.CreateParameter("@ALUNCPF", cpf);
-                cmd.Parameters.CreateParameter("@MATRICULA", matriculaDesconsiderar.HasValue ? matriculaDesconsiderar.Value : DBNull.Value);
+            cmd.Parameters.CreateParameter("@ALUNCPF", cpf);
+            cmd.Parameters.CreateParameter("@MATRICULA", matriculaDesconsiderar.HasValue ? matriculaDesconsiderar.Value : DBNull.Value);
 
-                using DbDataReader dr = cmd.ExecuteReader();
-                return dr.Read() && dr.GetInt64(0) > 0;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível verificar a existência do CPF.", ex);
-            }
+            using DbDataReader dr = cmd.ExecuteReader();
+            return dr.Read() && dr.GetInt64(0) > 0;
         }
     }
 }
--- EM.Repository/Repositories/RepositorioCidade.cs
+++ EM.Repository/Repositories/RepositorioCidade.cs
@@ -12,167 +12,111 @@ namespace EM.Repository.Repositories
     {
         public void Add(Cidade cidade)
         {
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText =
+            cmd.CommandText =
                     @"INSERT INTO TBCIDADE (CIDADESCRICAO, CIDAUF, CIDACODIGOIBGE)
                                        VALUES (@CIDADESCRICAO, @CIDAUF, @CIDACODIGOIBGE)";
 
-                cmd.Parameters.CreateParameter("@CIDADESCRICAO", cidade.Descricao);
-                cmd.Parameters.CreateParameter("@CIDAUF", cidade.EnumeradorUF);
+            cmd.Parameters.CreateParameter("@CIDADESCRICAO", cidade.Descricao);
+            cmd.Parameters.CreateParameter("@CIDAUF", cidade.EnumeradorUF);
 
-                cmd.Parameters.CreateParameter("@CIDACODIGOIBGE", cidade.CodigoIBGE > 0 
+            cmd.Parameters.CreateParameter("@CIDACODIGOIBGE", cidade.CodigoIBGE > 0 
                     ? cidade.CodigoIBGE
                     : DBNull.Value);
 
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível adicionar a cidade.", ex);
-            }
+            cmd.ExecuteNonQuery();
         }
 
         public void Remove(Cidade cidade)
         {
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText = "DELETE FROM TBCIDADE WHERE CIDACODIGO = @CIDACODIGO";
+            cmd.CommandText = "DELETE FROM TBCIDADE WHERE CIDACODIGO = @CIDACODIGO";
 
-                cmd.Parameters.CreateParameter("@CIDACODIGO", cidade.Codigo);
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível remover a cidade.", ex);
-            }
+            cmd.Parameters.CreateParameter("@CIDACODIGO", cidade.Codigo);
+            cmd.ExecuteNonQuery();
         }
 
         public void Update(Cidade cidade)
         {
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText = @"UPDATE TBCIDADE SET
+            cmd.CommandText = @"UPDATE TBCIDADE SET
                                        CIDADESCRICAO = @CIDADESCRICAO,
                                        CIDAUF = @CIDAUF,
                                        CIDACODIGOIBGE = @CIDACODIGOIBGE
                                        WHERE CIDACODIGO = @CIDACODIGO";
 
-                cmd.Parameters.CreateParameter("@CIDACODIGO", cidade.Codigo);
-                cmd.Parameters.CreateParameter("@CIDADESCRICAO", cidade.Descricao);
-                cmd.Parameters.CreateParameter("@CIDAUF", cidade.EnumeradorUF);
+            cmd.Parameters.CreateParameter("@CIDACODIGO", cidade.Codigo);
+            cmd.Parameters.CreateParameter("@CIDADESCRICAO", cidade.Descricao);
+            cmd.Parameters.CreateParameter("@CIDAUF", cidade.EnumeradorUF);
 
-                cmd.Parameters.CreateParameter("@CIDACODIGOIBGE", cidade.CodigoIBGE > 0
-                    ? cidade.CodigoIBGE
-                    : DBNull.Value);
+            cmd.Parameters.CreateParameter("@CIDACODIGOIBGE", cidade.CodigoIBGE > 0
+                ? cidade.CodigoIBGE
+                : DBNull.Value);
 
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível atualizar a cidade.", ex);
-            }
+            cmd.ExecuteNonQuery();
         }
 
         public IEnumerable<Cidade> GetAll()
         {
-            try
+            List<Cidade> cidades = [];
+
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
+
+            cmd.CommandText = @"SELECT CIDACODIGO, CIDADESCRICAO, CIDAUF, CIDACODIGOIBGE FROM TBCIDADE";
+
+            using DbDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
             {
-                List<Cidade> cidades = [];
-
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
-
-                cmd.CommandText = @"SELECT CIDACODIGO, CIDADESCRICAO, CIDAUF, CIDACODIGOIBGE FROM TBCIDADE";
-
-                using DbDataReader dr = cmd.ExecuteReader();
-                while (dr.Read())
+                Cidade cidade = new()
                 {
-                    Cidade cidade = new()
-                    {
-                        Codigo = dr.GetInt32(dr.GetOrdinal("CIDACODIGO")),
-                        Descricao = dr.GetString(dr.GetOrdinal("CIDADESCRICAO")),
-                        EnumeradorUF = (EnumeradorUF)dr.GetInt32(dr.GetOrdinal("CIDAUF")),
-                        CodigoIBGE = dr.IsDBNull(dr.GetOrdinal("CIDACODIGOIBGE"))
+                    Codigo = dr.GetInt32(dr.GetOrdinal("CIDACODIGO")),
+                    Descricao = dr.GetString(dr.GetOrdinal("CIDADESCRICAO")),
+                    EnumeradorUF = (EnumeradorUF)dr.GetInt32(dr.GetOrdinal("CIDAUF")),
+                    CodigoIBGE = dr.IsDBNull(dr.GetOrdinal("CIDACODIGOIBGE"))
                         ? null
                         : dr.GetInt32(dr.GetOrdinal("CIDACODIGOIBGE"))
-                    };
-
-                    cidades.Add(cidade);
-                }
-                return cidades;
+                };
+                cidades.Add(cidade);
             }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível obter todas as cidades.", ex);
-            }
+            return cidades;
         }
 
         public Cidade GetByCodigo(int cidadeId)
         {
-            try
-            {
-                Cidade? cidade = GetAll().FirstOrDefault(c => c.Codigo == cidadeId);
-                return cidade ?? throw new InvalidOperationException("Não foi possível encontrar a cidade pelo ID");
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível obter a cidade pelo ID", ex);
-            }
+            Cidade? cidade = GetAll().FirstOrDefault(c => c.Codigo == cidadeId);
+            return cidade ?? throw new InvalidOperationException("Não foi possível encontrar a cidade pelo ID");
         }
 
         public IEnumerable<Cidade> Get(Expression<Func<Cidade, bool>> predicate)
         {
-            try
-            {
-                IEnumerable<Cidade> cidades = GetAll().Where(predicate.Compile());
-                return cidades;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível obter as cidades por predicado", ex);
-            }
+            IEnumerable<Cidade> cidades = GetAll().Where(predicate.Compile());
+            return cidades;
         }
+        
 
         public IEnumerable<Cidade> GetByNome(string nomeCidade)
         {
-            try
-            {
-                IEnumerable<Cidade> cidades = GetAll().Where(c => c.Descricao.Contains(nomeCidade, StringComparison.OrdinalIgnoreCase));
-                return cidades;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível obter as cidades por parte do nome", ex);
-            }
+            IEnumerable<Cidade> cidades = GetAll().Where(c => c.Descricao.Contains(nomeCidade, StringComparison.OrdinalIgnoreCase));
+            return cidades;
         }
-
+         
         public bool PossuiRegistro(int codigo)
         {
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText = "SELECT COUNT(*) FROM TBALUNO WHERE CIDACODIGO = @CIDACODIGO";
-                cmd.Parameters.CreateParameter("@CIDACODIGO", codigo);
+            cmd.CommandText = "SELECT COUNT(*) FROM TBALUNO WHERE CIDACODIGO = @CIDACODIGO";
+            cmd.Parameters.CreateParameter("@CIDACODIGO", codigo);
 
-                using DbDataReader dr = cmd.ExecuteReader();
-                return dr.Read() && dr.GetInt64(0) > 0;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível obter registros por código", ex);
-            }
+            using DbDataReader dr = cmd.ExecuteReader();
+            return dr.Read() && dr.GetInt64(0) > 0;
         }
 
         public bool DescricaoExiste(string? descricao, int? codigoDesconsiderar = null)
@@ -180,53 +124,41 @@ namespace EM.Repository.Repositories
             if (string.IsNullOrWhiteSpace(descricao))
                 return false;
 
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+           
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText = @"SELECT COUNT(1) 
+            cmd.CommandText = @"SELECT COUNT(1) 
                                     FROM TBCIDADE
                                     WHERE UPPER(CIDADESCRICAO) = UPPER(@DESCRICAO)
                                     AND (@CODIGO IS NULL OR CIDACODIGO <> @CODIGO)";
 
-                cmd.Parameters.CreateParameter("@DESCRICAO", descricao);
-                cmd.Parameters.CreateParameter("@CODIGO", codigoDesconsiderar.HasValue ? codigoDesconsiderar.Value : DBNull.Value);
+            cmd.Parameters.CreateParameter("@DESCRICAO", descricao);
+            cmd.Parameters.CreateParameter("@CODIGO", codigoDesconsiderar.HasValue ? codigoDesconsiderar.Value : DBNull.Value);
 
-                using DbDataReader dr = cmd.ExecuteReader();
-                return dr.Read() && dr.GetInt64(0) > 0;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível verificar a existência da cidade.", ex);
-            }
+            using DbDataReader dr = cmd.ExecuteReader();
+            return dr.Read() && dr.GetInt64(0) > 0;
         }
-
+            
         public bool CodigoIbgeExiste(int? codigoIbge, int? codigoDesconsiderar = null)
         {
             if (!codigoIbge.HasValue)
                 return false;
 
-            try
-            {
-                using DbConnection cn = DBHelper.CriarConexao();
-                using DbCommand cmd = cn.CreateCommand();
+            
+            using DbConnection cn = DBHelper.CriarConexao();
+            using DbCommand cmd = cn.CreateCommand();
 
-                cmd.CommandText = @"SELECT COUNT(1)
+            cmd.CommandText = @"SELECT COUNT(1)
                                     FROM TBCIDADE
                                     WHERE CIDACODIGOIBGE = @CIDACODIGOIBGE
                                     AND (@CODIGO IS NULL OR CIDACODIGO <> @CODIGO)";
 
-                cmd.Parameters.CreateParameter("@CIDACODIGOIBGE", codigoIbge.Value);
-                cmd.Parameters.CreateParameter("@CODIGO", codigoDesconsiderar.HasValue ? codigoDesconsiderar.Value : DBNull.Value);
+            cmd.Parameters.CreateParameter("@CIDACODIGOIBGE", codigoIbge.Value);
+            cmd.Parameters.CreateParameter("@CODIGO", codigoDesconsiderar.HasValue ? codigoDesconsiderar.Value : DBNull.Value);
 
-                using DbDataReader dr = cmd.ExecuteReader();
-                return dr.Read() && dr.GetInt64(0) > 0;
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Não foi possível verificar a existência do código IBGE.", ex);
-            }
-        }
+            using DbDataReader dr = cmd.ExecuteReader();
+            return dr.Read() && dr.GetInt64(0) > 0;
+        } 
     }
 }
